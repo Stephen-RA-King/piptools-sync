a027beefde334d7f2193a9b7b3e210bb7bf1a051
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2022-08-24 17:36:46 +0100

    chore: initial commit of hook file

--- src/piptools_sync/__init__.py
+++ src/piptools_sync/__init__.py
@@ -1,13 +1,24 @@
 """Top-level package for piptools_sync."""
 # Core Library modules
-import configparser
-import json
 import logging.config
 from importlib import resources
+from pathlib import Path
 
 # Third party modules
+import git
 import toml  # type: ignore
 import yaml  # type: ignore
+
+__title__ = "piptools-sync"
+__version__ = "0.1.0"
+__author__ = "Stephen R A King"
+__description__ = "A piptools pre-commit version sync utility"
+__email__ = "stephen.ra.king@gmail.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022 Stephen R A King"
+
+
+ROOT_DIR = Path(git.Repo(".", search_parent_directories=True).working_tree_dir)
 
 LOGGING_CONFIG = """
 version: 1
@@ -43,26 +54,9 @@ loggers:
 logging.config.dictConfig(yaml.safe_load(LOGGING_CONFIG))
 logger = logging.getLogger("init")
 
-_yaml_text = resources.read_text("piptools_sync", "config.yaml")
-yaml_config = yaml.safe_load(_yaml_text)
+with resources.path("piptools_sync", "mapping.json") as path:
+    MAPPING_FILE = path
 
-_json_text = resources.read_text("piptools_sync", "config.json")
-json_config = json.loads(_json_text)
-
-_ini_text = resources.read_text("piptools_sync", "config.ini")
-_ini = configparser.ConfigParser()
-_ini.optionxform = str  # type: ignore
-_ini.read_string(_ini_text)
-ini_config = {section: dict(_ini.items(section)) for section in _ini.sections()}
 
 _toml_text = resources.read_text("piptools_sync", "config.toml")
 toml_config = toml.loads(_toml_text)
-
-
-__title__ = "piptools-sync"
-__version__ = "0.1.0"
-__author__ = "Stephen R A King"
-__description__ = "A piptools pre-commit version sync utility"
-__email__ = "stephen.ra.king@gmail.com"
-__license__ = "MIT"
-__copyright__ = "Copyright 2022 Stephen R A King"
