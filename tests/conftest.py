602e2c767da17dc5eb321a6de120a5c8f4e9e4c9
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2022-08-26 18:00:22 +0100

    chore: add fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,22 @@
-"""Put your fixtures for pytest here"""
+# Core Library modules
+from pathlib import Path
+from typing import Any
+
+# Third party modules
+import pytest
+
+# First party modules
+from piptools_sync import piptools_sync
+
+
+def pytest_configure() -> None:
+    pytest.TEST_DIR = Path(__file__).parent
+
+
+@pytest.fixture
+def mock_get_precommit_repos(monkeypatch: Any) -> None:
+    def mock_get_precommitrepos() -> list[list]:
+        repos = [["https://github.com/pre-commit/mirrors-mypy", "mypy"]]
+        return repos
+
+    monkeypatch.setattr(piptools_sync, "get_precommit_repos", mock_get_precommitrepos)
