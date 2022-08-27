620d4c2d155130bca5e5c3603cb10d72613c0f8a
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2022-08-27 11:48:23 +0100

    chore: add fixture for get_latest_github_repo_version

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,3 +20,15 @@ def mock_get_precommit_repos(monkeypatch: Any) -> None:
         return repos
 
     monkeypatch.setattr(piptools_sync, "get_precommit_repos", mock_get_precommitrepos)
+
+
+def mock_get_latest_github_repo_version(monkeypatch: Any) -> None:
+    def mock_get_latestgithubrepoversion() -> str:
+        version = "0.1.0"
+        return version
+
+    monkeypatch.setattr(
+        piptools_sync,
+        "get_latest_github_repo_version",
+        mock_get_latestgithubrepoversion,
+    )
